fe52c346829cb77322810072b0182a14b6bac0cd
IssaniRania <raniaissani83@gmail.com>
2023-10-28 20:08:50 +0100

    autorisation

--- app/src/main/java/com/example/platform_education/AdminActivity.kt
+++ app/src/main/java/com/example/platform_education/AdminActivity.kt
@@ -1,5 +1,8 @@
 package com.example.platform_education
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -9,8 +12,15 @@ import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
+import android.os.Build
+//import android.content.Context
+//import android.content.Intent
+//import android.app.NotificationChannel
+//import android.app.NotificationManager
+//import androidx.core.app.NotificationCompat
+//import android.app.PendingIntent
 class AdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,7 +63,7 @@ class AdminActivity : AppCompatActivity() {
             val profileMenuItem = bottomNavigation.menu.findItem(R.id.profile)
             profileMenuItem.title = "$username"
         }
-
+//partie acceptation
         for (i in 1..userCount) {
             val cin = sharedPrefs.getString("numero$i", "")
             val nomUtilisateur = sharedPrefs.getString("Name$i", "")
@@ -81,18 +91,53 @@ class AdminActivity : AppCompatActivity() {
             tableLayout.addView(row)
 
             acceptButton.setOnClickListener {
-                // Traitement à effectuer lorsque le bouton "Accepter" est cliqué
                 val editor = sharedPrefs.edit()
-                editor.putInt("etat", 1)
-                Toast.makeText(this@AdminActivity, "accepter", Toast.LENGTH_SHORT).show()
+                editor.putInt("Etat$i", 1)
+                editor.apply()
+                val username = sharedPrefs.getString("Name$i", "") // Remplacez par la vraie source de données
+
+                if (username != null) {
+
+                    sendNotificationToUser(username)
+                    Toast.makeText(this@AdminActivity, "accepter", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@AdminActivity, "Impossible de trouver l'e-mail de l'utilisateur", Toast.LENGTH_SHORT).show()
+                }
             }
 
             refuseButton.setOnClickListener {
                 // Traitement à effectuer lorsque le bouton "Refuser" est cliqué
                 val editor = sharedPrefs.edit()
-                editor.putInt("etat", 0)
+                editor.putInt("Etat$i", 0)
+                editor.apply()
                 Toast.makeText(this@AdminActivity, "refuser", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+    //partie du notification
+    private fun sendNotificationToUser(username: String) {
+        // Utilisez l'e-mail de l'utilisateur pour personnaliser le contenu de la notification
+        val notificationContent = "Bienvenue, $username ! Vous pouvez maintenant accéder à votre compte."
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("mon_canal_id", "Mon Canal", NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = "Description du canal"
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val builder = NotificationCompat.Builder(this, "mon_canal_id")
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("Accès autorisé")
+            .setContentText(notificationContent)
+            .setAutoCancel(true)
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        builder.setContentIntent(pendingIntent)
+
+        val notificationId = 1
+        notificationManager.notify(notificationId, builder.build())
     }
 }
